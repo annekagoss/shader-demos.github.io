6b05a6bdb0086649bc8bb39381072c1d3ed61890
Anneka Goss <annekagoss@gmail.com>
2019-12-02 21:12:47 -0500

    frag/vert tabs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,36 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { SceneProps } from '../../types';
-import GLScene from './GLScene/GLScene';
-import Section from './Section/Section';
-import BaseCanvas from './BaseCanvas/BaseCanvas';
-import ShaderText from './ShaderText/ShaderText';
-import baseVertexShader from '../../lib/gl/shaders/base.vert';
+import { SceneProps } from "../../types";
+import GLScene from "./GLScene/GLScene";
+import Section from "./Section/Section";
+import BaseCanvas from "./BaseCanvas/BaseCanvas";
+import ShaderText from "./ShaderText/ShaderText";
+import baseVertexShader from "../../lib/gl/shaders/base.vert";
 
 // HELLO WORlD
-import helloWorldFragmentShader from '../../lib/gl/shaders/hello-world.frag';
+import helloWorldFragmentShader from "../../lib/gl/shaders/hello-world.frag";
 
-import styles from './app.module.scss';
+import styles from "./app.module.scss";
 
 // FOX SKULL
-import OBJSource from '../../lib/gl/assets/fox/fox3.obj';
-import MTLSource from '../../lib/gl/assets/fox/fox.mtl';
-import diffuseSource0 from '../../lib/gl/assets/fox/fox_skull_0.jpg';
-import diffuseSource1 from '../../lib/gl/assets/fox/fox_skull_1.jpg';
+import OBJSource from "../../lib/gl/assets/fox/fox3.obj";
+import MTLSource from "../../lib/gl/assets/fox/fox.mtl";
+import diffuseSource0 from "../../lib/gl/assets/fox/fox_skull_0.jpg";
+import diffuseSource1 from "../../lib/gl/assets/fox/fox_skull_1.jpg";
 
 const GL_PROPS: SceneProps = {
   colors: {
-    ambientLight: 'rgba(100, 100, 100, 1)',
-    backgroundA: 'rgba(255, 0, 0, 1)',
-    backgroundB: 'rgba(0, 0, 255, 1)',
-    leftLight: 'rgba(150, 150, 150, 1)',
-    rightLight: 'rgba(150, 150, 150, 1)'
+    ambientLight: "rgba(100, 100, 100, 1)",
+    backgroundA: "rgba(255, 0, 0, 1)",
+    backgroundB: "rgba(0, 0, 255, 1)",
+    leftLight: "rgba(150, 150, 150, 1)",
+    rightLight: "rgba(150, 150, 150, 1)"
   },
   OBJSource,
   MTLSource,
   diffuseSources: {
-    'material_0.001': diffuseSource0,
-    'material_1.001': diffuseSource1
+    "material_0.001": diffuseSource0,
+    "material_1.001": diffuseSource1
   },
   positionOffset: {
     x: 0,
@@ -57,9 +57,9 @@ export default class App extends React.Component {
           {/* <GLScene {...GL_PROPS} /> */}
 
           <Section
-            title='0.0: Hello World'
+            title="0.0: Hello World"
             notes={`
-              The fragment shader is rendered onto a base mesh. In these first examples we will use a 1x1 plane which acts like a projection screen.
+              The fragment shader is rendered onto a base mesh. In these first examples we will use a 1x1 plane which acts as a projection screen.
               The aVertexPosition attribute holds an array of 3-vector coordinates for each vertex of the base mesh.  It looks like this: [-1, 1, 0, 1, 1, 0, -1, -1, 0, 1, -1, 0]
             `}
           >
--- src/components/ShaderText/ShaderText.tsx
+++ src/components/ShaderText/ShaderText.tsx
@@ -1,22 +1,52 @@
-import * as React from 'react';
-import styles from './ShaderText.module.scss';
+import * as React from "react";
+import cx from "classnames";
+import styles from "./ShaderText.module.scss";
 
 interface Props {
   fragmentShader: string;
   vertexShader: string;
 }
 
-const ShaderText = ({ fragmentShader, vertexShader }: Props) => (
-  <div className={styles.root}>
-    <div className={styles.textContainer}>
-      <div className={styles.title}>Fragment Shader</div>
-      <div className={styles.textBlock}>{fragmentShader}</div>
+const ShaderText = ({ fragmentShader, vertexShader }: Props) => {
+  const [fragVisible, setFragVisible] = React.useState<boolean>(true);
+  const [vertVisible, setVertVisible] = React.useState<boolean>(false);
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.tabs}>
+        <button
+          className={cx(styles.tab, fragVisible && styles.active)}
+          onClick={() => {
+            if (!fragVisible) {
+              setFragVisible(true);
+              setVertVisible(false);
+            }
+          }}
+        >
+          Fragment Shader
+        </button>
+        <button
+          className={cx(styles.tab, vertVisible && styles.active)}
+          onClick={() => {
+            if (!vertVisible) {
+              setVertVisible(true);
+              setFragVisible(false);
+            }
+          }}
+        >
+          Vertex Shader
+        </button>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={cx(styles.textBlock, fragVisible && styles.active)}>
+          {fragmentShader}
+        </div>
+        <div className={cx(styles.textBlock, vertVisible && styles.active)}>
+          {vertexShader}
+        </div>
+      </div>
     </div>
-    <div className={styles.textContainer}>
-      <div className={styles.title}>Vertex Shader</div>
-      <div className={styles.textBlock}>{vertexShader}</div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ShaderText;
