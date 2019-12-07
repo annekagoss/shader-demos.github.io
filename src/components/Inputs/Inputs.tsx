8f621df18374c9d5427fdb7d8383e51e745bdae9
Anneka Goss <annekagoss@gmail.com>
2019-12-07 17:47:34 -0500

    cleanup

--- src/components/Inputs/Inputs.tsx
+++ src/components/Inputs/Inputs.tsx
@@ -1,65 +1,76 @@
-import * as React from "react";
-import cx from "classnames";
-import styles from "./Inputs.module.scss";
+import * as React from 'react';
+import cx from 'classnames';
+import styles from './Inputs.module.scss';
 
 interface Props {
-  attributes: any[];
-  uniforms: any[];
+	attributes: any[];
+	uniforms: any[];
 }
 
 const Inputs = ({ uniforms, attributes }: Props) => {
-  const [uniformsVisible, setUniformsVisible] = React.useState<boolean>(true);
-  const [attributesVisible, setAttributesVisible] = React.useState<boolean>(
-    false
-  );
-  console.log(attributes);
+	const [uniformsVisible, setUniformsVisible] = React.useState<boolean>(true);
+	const [attributesVisible, setAttributesVisible] = React.useState<boolean>(
+		false
+	);
+	console.log(attributes);
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.tabs}>
-        <button
-          className={cx(styles.tab, uniformsVisible && styles.active)}
-          onClick={() => {
-            if (!uniformsVisible) {
-              setUniformsVisible(true);
-              setAttributesVisible(false);
-            }
-          }}
-        >
-          Uniforms
-        </button>
-        <button
-          className={cx(styles.tab, attributesVisible && styles.active)}
-          onClick={() => {
-            if (!attributesVisible) {
-              setAttributesVisible(true);
-              setUniformsVisible(false);
-            }
-          }}
-        >
-          Attributes
-        </button>
-      </div>
-      <div className={styles.textContainer}>
-        <div className={cx(styles.textBlock, uniformsVisible && styles.active)}>
-          {uniforms.map(uniform => (
-            <div className={styles.textItem} key={uniform.name}>
-              {uniform.name}: {uniform.value}
-            </div>
-          ))}
-        </div>
-        <div
-          className={cx(styles.textBlock, attributesVisible && styles.active)}
-        >
-          {attributes.map(attribute => (
-            <div className={styles.textItem} key={attribute.name}>
-              {attribute.name}: [{attribute.value}]
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.root}>
+			<div className={styles.tabs}>
+				<button
+					className={cx(styles.tab, uniformsVisible && styles.active)}
+					onClick={() => {
+						if (!uniformsVisible) {
+							setUniformsVisible(true);
+							setAttributesVisible(false);
+						}
+					}}
+				>
+					Uniforms
+				</button>
+				<button
+					className={cx(
+						styles.tab,
+						attributesVisible && styles.active
+					)}
+					onClick={() => {
+						if (!attributesVisible) {
+							setAttributesVisible(true);
+							setUniformsVisible(false);
+						}
+					}}
+				>
+					Attributes
+				</button>
+			</div>
+			<div className={styles.textContainer}>
+				<div
+					className={cx(
+						styles.textBlock,
+						uniformsVisible && styles.active
+					)}
+				>
+					{uniforms.map(uniform => (
+						<div className={styles.textItem} key={uniform.name}>
+							{uniform.name}: {uniform.value}
+						</div>
+					))}
+				</div>
+				<div
+					className={cx(
+						styles.textBlock,
+						attributesVisible && styles.active
+					)}
+				>
+					{attributes.map(attribute => (
+						<div className={styles.textItem} key={attribute.name}>
+							{attribute.name}: [{attribute.value}]
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Inputs;
--- src/components/ShaderText/ShaderText.tsx
+++ src/components/ShaderText/ShaderText.tsx
@@ -1,64 +1,74 @@
-import * as React from "react";
-import cx from "classnames";
-import * as prism from "prismjs";
-import "prismjs/components/prism-glsl";
+import * as React from 'react';
+import cx from 'classnames';
+import * as prism from 'prismjs';
+import 'prismjs/components/prism-glsl';
 
-import "../prism.css";
-import styles from "./ShaderText.module.scss";
+import '../prism.css';
+import styles from './ShaderText.module.scss';
 
 interface Props {
-  fragmentShader: string;
-  vertexShader: string;
+	fragmentShader: string;
+	vertexShader: string;
 }
 
 const ShaderText = ({ fragmentShader, vertexShader }: Props) => {
-  const [fragVisible, setFragVisible] = React.useState<boolean>(true);
-  const [vertVisible, setVertVisible] = React.useState<boolean>(false);
+	const [fragVisible, setFragVisible] = React.useState<boolean>(true);
+	const [vertVisible, setVertVisible] = React.useState<boolean>(false);
 
-  React.useEffect(() => {
-    prism.highlightAll();
-  }, []);
+	React.useEffect(() => {
+		prism.highlightAll();
+	}, []);
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.tabs}>
-        <button
-          className={cx(styles.tab, fragVisible && styles.active)}
-          onClick={() => {
-            if (!fragVisible) {
-              setFragVisible(true);
-              setVertVisible(false);
-            }
-          }}
-        >
-          Fragment Shader
-        </button>
-        <button
-          className={cx(styles.tab, vertVisible && styles.active)}
-          onClick={() => {
-            if (!vertVisible) {
-              setVertVisible(true);
-              setFragVisible(false);
-            }
-          }}
-        >
-          Vertex Shader
-        </button>
-      </div>
-      <div className={styles.textContainer}>
-        <div className={cx(styles.textBlock, fragVisible && styles.active)}>
-          <pre>
-            <code className="language-glsl">{fragmentShader}</code>
-          </pre>
-        </div>
-        <div className={cx(styles.textBlock, vertVisible && styles.active)}>
-          <pre>
-            <code className="language-glsl">{vertexShader}</code>
-          </pre>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.root}>
+			<div className={styles.tabs}>
+				<button
+					className={cx(styles.tab, fragVisible && styles.active)}
+					onClick={() => {
+						if (!fragVisible) {
+							setFragVisible(true);
+							setVertVisible(false);
+						}
+					}}
+				>
+					Fragment Shader
+				</button>
+				<button
+					className={cx(styles.tab, vertVisible && styles.active)}
+					onClick={() => {
+						if (!vertVisible) {
+							setVertVisible(true);
+							setFragVisible(false);
+						}
+					}}
+				>
+					Vertex Shader
+				</button>
+			</div>
+			<div className={styles.textContainer}>
+				<div
+					className={cx(
+						styles.textBlock,
+						fragVisible && styles.active
+					)}
+				>
+					<pre>
+						<code className='language-glsl'>{fragmentShader}</code>
+					</pre>
+				</div>
+				<div
+					className={cx(
+						styles.textBlock,
+						vertVisible && styles.active
+					)}
+				>
+					<pre>
+						<code className='language-glsl'>{vertexShader}</code>
+					</pre>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ShaderText;
