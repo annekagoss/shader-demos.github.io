ef7b181605535ffebf19edbf1adc703627a9a529
Anneka Goss <annekagoss@gmail.com>
2020-03-22 17:11:43 -0400

    safari

--- lib/gl/shaders/common/normal-color.glsl
+++
@@ -1,8 +0,0 @@
-vec3 normalColor(vec3 normal) {
-  float x = (normal.x * .5) + .5;
-  float y = (normal.y * .5) + .5;
-  float z = (normal.z * .5) + .5;
-  return vec3(x, y, z);
-}
-
-#pragma glslify: export(normalColor) 
