44f4654f13760d8083f34de7d79674aec6b71bdb
Anneka Goss <annekagoss@gmail.com>
2020-03-22 16:13:41 -0400

    webworker test

--- src/hooks/webWorker.ts
+++ src/hooks/webWorker.ts
@@ -1,12 +1,12 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import loadOBJWorker from '../../lib/gl/loadOBJWorker';
 import WebWorker from '../../lib/gl/WebWorker';
-import {Mesh, Textures, WebWorkerLoadMessage} from '../../types';
+import { Mesh, Textures, WebWorkerLoadMessage } from '../../types';
 
-export const useOBJLoaderWebWorker = ({onLoadHandler, OBJData}: WebWorkerLoadMessage) => {
+export const useOBJLoaderWebWorker = ({ onLoadHandler, OBJData }: WebWorkerLoadMessage) => {
 	useEffect(() => {
 		const worker: Worker = new WebWorker(loadOBJWorker) as Worker;
-		worker.addEventListener('message', (event: {data: Mesh}) => onLoadHandler(event.data));
-		worker.postMessage(OBJData);
+		worker.addEventListener('message', (event: { data: Mesh }) => onLoadHandler(event.data));
+		worker.postMessage(OBJData, undefined);
 	}, []);
 };
