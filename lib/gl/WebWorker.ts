c464c8b17a940ced4e5e629dfd9f3b1200159e09
Anneka Goss <annekagoss@gmail.com>
2020-03-22 15:29:25 -0400

    webpack tests

--- lib/gl/WebWorker.ts
+++
@@ -1,7 +0,0 @@
-export default class WebWorker {
-	constructor(worker: Function) {
-		const code: string = worker.toString();
-		const blob: Blob = new Blob(['(' + code + ')()']);
-		return new Worker(URL.createObjectURL(blob));
-	}
-}
--- lib/gl/shaders/base.vert
+++
@@ -1,6 +0,0 @@
-precision mediump float;
-
-attribute vec3 aBaseVertexPosition;
-void main() {
-    gl_Position = vec4(aBaseVertexPosition, 1.0);
-}
